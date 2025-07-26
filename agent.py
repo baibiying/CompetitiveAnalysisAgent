b40895e76c9bf93f955478982b35b522df9fd83e
baibiying <baibiying@icloud.com>
2025-07-26 15:25:19 +0800

    fix market_price_range

--- agent.py
+++ agent.py
@@ -202,7 +202,7 @@ def perform_final_analysis(product_name, price, price_unit=None, image_url=None,
                         "5. 营养成分分析(nutrition_analysis:str), 要求必须返回至少一个营养成分，营养成分和相应的营养成分含量，形式如”维生素a-100mg-维生素b-200mg-维生素c-300mg“。\n"
                         "6. 产品整体描述（description:str）\n"
                         "营养成分分析最多35个字，其他分析部分限15个字以内。"
-                        "最终以JSON格式返回，字段包括：product_name, price, masket_price_range, is_overpriced, fresh_level, sweet_level, sour_level, water_level, crisp_level, description, price_analysis, price_unit, advantage_analysis, disadvantage_analysis, nutrition_analysis"
+                        "最终以JSON格式返回，字段包括：product_name, price, market_price_range, is_overpriced, fresh_level, sweet_level, sour_level, water_level, crisp_level, description, price_analysis, price_unit, advantage_analysis, disadvantage_analysis, nutrition_analysis"
                     )},
                    # {"type": "image_url", "image_url": {"url": image_url}}
                 ]
